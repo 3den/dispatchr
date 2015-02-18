ea987f59fe9d681f75dfbe2e5f92c18e14095482
Marcelo Eden <eden@3den.biz>
2015-02-17 20:54:02 -0800

    suggestion for updating the index

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
  * Copyright 2014, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
-module.exports = require('./lib/Dispatcher');
+module.exports = {
+  createDispatcher: require('./lib/createDispatcher'),
+  createStore: require('./utils/createStore'),
+  Action: require('./lib/Action')
+};
